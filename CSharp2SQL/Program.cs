a82ebf033c3b785b2f5a588d432d389848b0cae4
Sarah George <sgeorge@tql.com>
2021-05-25 15:01:08 -0400

    Add project files.

---
+++ CSharp2SQL/Program.cs
@@ -0,0 +1,15 @@
+﻿using CSharp2SQLLib;
+using System;
+
+namespace CSharp2SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sqllib = new SqlLib();
+            sqllib.Connect();
+            sqllib.Disconnet();
+        }
+    }
+}
---
+++ CSharp2SQLLib/SqlLib.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace CSharp2SQLLib
+{
+    public class SqlLib
+    {
+        public SqlConnection sqlconn { get; set; }
+
+        public void Connect()
+        {
+            var connStr = "server=localhost\\sqlexpress01;" +
+                            "database=EdDb;" +
+                            "trusted_connection=true;";
+            sqlconn = new SqlConnection(connStr);
+            sqlconn.Open();
+            if (sqlconn.State != System.Data.ConnectionState.Open)
+            {
+                throw new Exception("Connection string is not correct!");
+            }
+            Console.WriteLine("Open connection successful!");
+        }
+            public void Disconnet()
+            {
+                if(sqlconn == null)
+                {
+                    return;
+                }
+            sqlconn.Close();
+            sqlconn = null;
+
+            }
+
+        
+
+    }
+}
